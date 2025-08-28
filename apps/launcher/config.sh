337e54b4ce3f980240f56496e473da4343fbf06c
valere <dev@valerebron.com>
2025-08-28 19:01:37 +0200

    clean apps nextcloud

--- apps/launcher/config.sh
+++ apps/launcher/config.sh
@@ -4,4 +4,4 @@ export PORT_EXPOSED="80"
 export REDIRECTIONS=""
 export POST_INSTALL_TEST_CMD="pwd"
 export LAUNCHER_HIDDEN=true
-export ALIASES="map->map.$MAIN_DOMAIN youtube->www.youtube.com chatgpt->chatgpt.com whatsapp->web.whatsapp.com"
+export ALIASES="tv->tv.$MAIN_DOMAIN map->mappy.com youtube->www.youtube.com chatgpt->chatgpt.com whatsapp->web.whatsapp.com"
