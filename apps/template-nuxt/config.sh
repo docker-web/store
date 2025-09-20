5c477e036308ed7101da84cab4f7c94255a6a4e7
valere <dev@valerebron.com>
2025-09-20 10:47:08 +0200

    add store & server for Nuxt

--- apps/template-nuxt/config.sh
+++ apps/template-nuxt/config.sh
@@ -1,5 +1,4 @@
-export REPO_NAME="__APP_NAME__"
-export DOMAIN="__APP_NAME__.$MAIN_DOMAIN"
-export PORT="__PORT__"
+export REPO_NAME="nuxtci"
+export DOMAIN="nuxtci.$MAIN_DOMAIN"
+export PORT="7903"
 export PORT_EXPOSED="3000"
-export REDIRECTIONS="" # example.$MAIN_DOMAIN->/route $MAIN_DOMAIN->url /route->/another-route /route->url
--- apps/template/README.md
+++ apps/template/README.md
@@ -1,6 +1,6 @@
 <h1 align="center">
   <picture>
-    <img align="center" alt="__APP_NAME__" src="./logo.svg" height="100">
+    <img align="center" alt="APP-NAME" src="./logo.svg" height="100">
   </picture>
-  __APP_NAME__
+  APP-NAME
 </h1>
