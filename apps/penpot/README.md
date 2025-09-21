a2d43497d8d3cd51fdd05a347302a970da76c0b9
valere <dev@valerebron.com>
2025-09-21 10:23:13 +0200

    penpot beta

---
+++ apps/penpot/README.md
@@ -0,0 +1,6 @@
+<h1 align="center">
+  <picture>
+    <img align="center" alt="APP-NAME" src="./logo.svg" height="100">
+  </picture>
+  APP-NAME
+</h1>
---
+++ apps/penpot/config.sh
@@ -0,0 +1,5 @@
+export DOMAIN="penpot.$MAIN_DOMAIN"
+export PORT="7904"
+export PORT_EXPOSED="8080"
+export SECRET_KEY=""
+export REDIRECTIONS="" # example.$MAIN_DOMAIN->/route $MAIN_DOMAIN->url /route->/another-route /route->url
